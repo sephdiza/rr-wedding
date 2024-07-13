bc8bd43a76a20b8f88a2377b21abb29611ef0e2f
Joseph Diza <joseph@wizy.io>
2024-07-13 19:38:58 +0800

    adjust backdrop brightness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <main className="text-white">
       <Navbar />
-      <div className="flex h-screen flex-col items-center justify-between bg-[url('../../public/heroimg.png')] bg-cover bg-center bg-no-repeat">
-        <div className="flex h-screen w-full flex-col items-center justify-between bg-[#145DA1]/30 py-12 backdrop-brightness-50">
-          <p className="pt-11 text-center text-9xl">
+      <div className="flex h-screen w-full flex-col items-center justify-between bg-[url('../../public/heroimg.png')] bg-cover bg-center bg-no-repeat">
+        <div className="flex h-screen w-full flex-col items-center justify-between bg-[#145DA1]/30 py-12 backdrop-brightness-[.3]">
+          <p className="pt-11 text-center text-8xl">
             <span className="block">Ron</span>
             <span className="block">&</span>
             <span className="block">Raineth</span>
           </p>
           <div>
-            <p className="text-xl">99H 99D 59M 59S</p>
+            <p className="text-xl">99D 59H 59M 59S</p>
           </div>
         </div>
       </div>
