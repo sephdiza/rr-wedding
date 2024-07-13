5899ff56c8962ec30d81c888bf8505d5e9b3cd0a
Joseph Diza <joseph@wizy.io>
2024-07-13 19:45:51 +0800

    add blur to navbar, add new section

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="fixed z-10 flex h-14 w-full items-center justify-between p-4">
+    <nav className="fixed z-10 flex h-14 w-screen items-center justify-between p-5 backdrop-blur-sm">
       <h1 className="text-lg">R | R</h1>
       <button className="">hamburger icon</button>
     </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
   return (
     <main className="text-white">
       <Navbar />
-      <div className="flex h-screen w-full flex-col items-center justify-between bg-[url('../../public/heroimg.png')] bg-cover bg-center bg-no-repeat">
-        <div className="flex h-screen w-full flex-col items-center justify-between bg-[#145DA1]/30 py-12 backdrop-brightness-[.3]">
+
+      <div className="flex h-screen w-screen flex-col items-center justify-between bg-[url('../../public/heroimg.png')] bg-cover bg-center bg-no-repeat">
+        <div className="flex h-screen w-screen flex-col items-center justify-between bg-[#145DA1]/30 py-12 backdrop-brightness-[.3]">
           <p className="pt-11 text-center text-8xl">
             <span className="block">Ron</span>
             <span className="block">&</span>
@@ -17,6 +18,13 @@ export default function Home() {
             <p className="text-xl">99D 59H 59M 59S</p>
           </div>
         </div>
+      </div>
+
+      <div className="min-h-44 text-black">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis itaque
+        distinctio repellat neque facere. Dolor impedit, commodi alias nesciunt
+        qui quod quis explicabo facere numquam magnam, sunt iure nisi
+        consequuntur?
       </div>
     </main>
   );
