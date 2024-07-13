ddab2ed7d4d887c633ede21f67224e0af5c8d602
Joseph Diza <joseph@wizy.io>
2024-07-13 19:02:05 +0800

    wip: hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-import Image from 'next/image';
+"use client";
+
+import Navbar from "./components/Navbar";
 
 export default function Home() {
-	return (
-		<main>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quasi
-			esse laudantium facere, consectetur voluptatibus temporibus! Eius dolore
-			minima modi quibusdam perspiciatis autem eligendi, sequi cupiditate
-			accusantium ipsam error? Natus?
-		</main>
-	);
+  return (
+    <main className="text-white">
+      <Navbar />
+      <div className="flex h-screen flex-col items-center justify-between bg-[url('../../public/heroimg.png')] bg-cover bg-center bg-no-repeat">
+        <div className="flex h-screen w-full flex-col items-center justify-between bg-[#145DA1]/30 py-12 backdrop-brightness-50">
+          <p className="pt-11 text-center text-9xl">
+            <span className="block">Ron</span>
+            <span className="block">&</span>
+            <span className="block">Raineth</span>
+          </p>
+          <div>
+            <p className="text-xl">99H 99D 59M 59S</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
