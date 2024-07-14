2031b160447356f066ada771c67187be7ccac7cb
Joseph Diza <joseph@wizy.io>
2024-07-14 09:07:27 +0800

    remove backdrop overlay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
       <Navbar />
 
       <div className="flex h-screen w-screen flex-col items-center justify-between overflow-hidden bg-[url('../../public/herov3.png')] bg-cover bg-center bg-no-repeat">
-        <div className="flex h-screen w-screen flex-col items-center justify-between py-12 backdrop-brightness-[.5]">
+        <div className="flex h-screen w-screen flex-col items-center justify-between py-12">
           <p className="font-hero pt-20 text-center text-6xl leading-relaxed">
-            <span className="block">Ron</span>
+            <span className="block">Ronald</span>
             <span className="block">&</span>
             <span className="block">Raineth</span>
           </p>
